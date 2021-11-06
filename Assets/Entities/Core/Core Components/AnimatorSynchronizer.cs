42d3d938f74c52e68c12ee0db3eef4a733b072c9
NowakArtur97 <nowart97@gmail.com>
2021-11-06 19:03:17 +0100

    Fix issue with Synchronizing Animations

--- Assets/Entities/Core/Core Components/AnimatorSynchronizer.cs
+++ Assets/Entities/Core/Core Components/AnimatorSynchronizer.cs
@@ -11,6 +11,13 @@ namespace NowakArtur97.LoopedDungeon.Core
 
         public void Synchronize() => _childAnimator.Play(0, -1, _masterAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
 
+        protected override void Awake()
+        {
+            base.Awake();
+
+            IsSynchronized = true;
+        }
+
         public override void LogicUpdate()
         {
             base.LogicUpdate();
